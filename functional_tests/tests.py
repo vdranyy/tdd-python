86d689d5c264ce4bb07f1f1a9e3d8177d1735a76
Vova Dranyy <v.dranyy@gmail.com>
2016-01-15 22:18:27 +0200

    extends functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,9 +36,12 @@ class NewVisitorTest(LiveServerTestCase):
         # tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page list
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, she is taken to a new URL,
+        # and now the page list "1: Buy peacock feathers" as an item in a
+        #  to-do list table
         inputbox.send_keys(Keys.ENTER)
+        tatiana_list_url = self.browser.current_url
+        self.assertRegexpMatches(tatiana_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -51,11 +54,35 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Tatiana wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect
+        # Now a new user, Vova, comes along to the site.
+
+        # # We use a new browser session to make sure that no
+        # # information of Tatiana's is coming through from cookies etc #
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Vova visits the home page. There's no sign of Tatiana's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text.text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text.text)
+
+        # Vova starts a new list by entering a new item. He
+        # is less interesting then Tatiana
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Vova gets his unique URL
+        vova_list_url = self.browser.current_url
+        self.assertRegexpMatches(vova_list_url, '/lists/.+')
+        self.assertNotEqual(vova_list_url, tatiana_list_url)
+
+        # Again, there is no trace of Tatiana's list
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text.text)
+        self.assertIn('Buy milk', page_text.text)
+
+        # Satisfied, they both go back to sleep
         self.fail('Incomplete test...')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep

f7c755666df3b455954037b61025e4d8c4853f8c
Vova Dranyy <v.dranyy@gmail.com>
2016-01-24 10:26:22 +0200

    Move unit tests into a folder with single file

--- lists/tests/test_all.py
+++ lists/tests/test_all.py
@@ -2,8 +2,8 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
-from .views import home_page
-from .models import Item, List
+from lists.views import home_page
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):

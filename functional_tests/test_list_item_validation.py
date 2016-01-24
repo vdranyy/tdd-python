cb2b93daa7c2a3bc9cc1dcc9bb964f6763a1fa16
Vova Dranyy <v.dranyy@gmail.com>
2016-01-24 14:22:17 +0200

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,4 @@
 from .base import FunctionalTest
-from selenium.webdriver.common.keys import Keys
 
 
 class ItemValidationTest(FunctionalTest):
@@ -8,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
         # Tatiana goes to the home page accidentally tries to submit
         # an empty list item. She hits enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # The home page refreshes, and there is an error message saying
         # that list item cannot be blank
@@ -16,12 +15,11 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, 'You can\'t have an empty list item')
 
         # She tries again with some text for the item, which now works
-        inputbox = self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decide submit a second blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
@@ -29,7 +27,6 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, 'You can\'t have an empty list item')
 
         # And she can correct it by filling some text in it
-        inputbox = self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')

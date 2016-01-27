bf1423e5576268ecc19a68c523c571fa7df2454b
Vova Dranyy <v.dranyy@gmail.com>
2016-01-27 23:24:51 +0200

    replace live_server_url to server_url, so that is work on staging server

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Tatiana goes to the home page accidentally tries to submit
         # an empty list item. She hits enter on the empty input box
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
 
         # The home page refreshes, and there is an error message saying
@@ -36,7 +36,7 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_duplicate_items(self):
         # Tatiana goes to the home page and starts a new list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('Buy wellies\n')
         self.check_for_row_in_list_table('1: Buy wellies')
 
@@ -50,7 +50,7 @@ class ItemValidationTest(FunctionalTest):
 
     def test_error_messages_are_cleared_on_input(self):
         # Tatiana starts a new list in a way that causes a validation error:
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
         error = self.get_error_element()
         self.assertTrue(error.is_displayed())

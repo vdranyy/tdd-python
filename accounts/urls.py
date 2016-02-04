e813041c1ca10e2a71b5ff872499f710c090bc52
Vova Dranyy <v.dranyy@gmail.com>
2016-02-04 20:18:05 +0200

    extends test_login functional test with 'logout'

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^login$', 'accounts.views.persona_login', name='persona_login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 ]
